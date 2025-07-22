6368be4295acaab643b860bf46896fdde094a437
EmanuelGaleno <emanuelnfs0@gmail.com>
2025-07-22 17:36:04 -0300

    refactor

--- src/main/java/bank/consult/springboot/repository/UserRepository.java
+++ src/main/java/bank/consult/springboot/repository/UserRepository.java
@@ -8,11 +8,16 @@ import java.util.List;
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
     // Metodo para verificar se user ja existe e dps criar uma conta
-    @Query("SELECT u FROM UserEntity u WHERE LOWER(u.firstName) = LOWER(:firstName) AND LOWER(u.lastName) = LOWER(:lastName)")
+    @Query("SELECT u FROM UserEntity u " +
+            "WHERE LOWER(u.firstName) = LOWER(:firstName) " +
+            "AND LOWER(u.lastName) = LOWER(:lastName)")
     List<UserEntity> findByFirstNameAndLastName(String firstName, String lastName);
 
     // Metodo para buscar usuario pelo primeiro nome, sobrenome e ID
-    @Query("SELECT u FROM UserEntity u WHERE LOWER(u.firstName) = LOWER(:firstName) AND LOWER(u.lastName) = LOWER(:lastName) AND u.id = :id")
+    @Query("SELECT u FROM UserEntity u " +
+            "WHERE LOWER(u.firstName) = LOWER(:firstName) " +
+            "AND LOWER(u.lastName) = LOWER(:lastName) " +
+            "AND u.id = :id")
     List<UserEntity> findByFirstNameAndLastNameAndId(String firstName, String lastName, Long id);
 }
 
